8012df85122bc73eb96d7c5f998888daeb1badf0
Hernán Burgos <122620259+burgosha@users.noreply.github.com>
2023-04-01 16:47:57 -0300

    Logo y estilos inciales

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import freeCodeCampLogo from './images/freecodecamp-logo.png';
 
 function App() {
   return (
-    <div className="App">
-      Hola
+    <div className='App'>
+      <div className='freecodecamp-logo-conenedor'>
+        <img 
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo'
+          alt='Logo de freeCodeCamp' />
+      </div>
     </div>
   );
 }
